72bdfc3fdc4a69e66c6047aff24b38d22d119fcd
JiweiHuang <huangjiwei@gxust.edu.cn>
2019-04-02 13:40:47 +0800

    Update 2019-04-01

--- src/main/kotlin/cn/edu/gxust/jiweihuang/kotlin/math/stat/random/RandomDataGenerator.kt
+++ src/main/kotlin/cn/edu/gxust/jiweihuang/kotlin/math/stat/random/RandomDataGenerator.kt
@@ -3,5 +3,4 @@ package cn.edu.gxust.jiweihuang.kotlin.math.stat.random
 class RandomDataGenerator : org.hipparchus.random.RandomDataGenerator {
     constructor() : super()
     constructor(seed: Long) : super(seed)
-
 }
